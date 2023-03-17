adc053ef4661121076829b7f01be9f7c54db0296
Samit Koyom <samitkoyom@gmail.com>
2023-03-17 14:30:42 +0700

    update welcome page

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -28,6 +28,12 @@
                     @endauth
                 </div>
             @endif
+
+        </div>
+
+        <div>
+            <h1>Home Page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, optio quod error nihil voluptate quas iure minus officia nam beatae ea nobis unde. Est facere perferendis delectus deserunt asperiores laudantium, pariatur iusto natus provident excepturi, cupiditate iste repellat magnam ducimus distinctio temporibus necessitatibus tempore fugit, adipisci quisquam nostrum! Voluptatibus, ullam quisquam sequi laborum sint molestiae. Repellendus molestias, deleniti repudiandae, mollitia dicta ullam quia possimus sit voluptas perspiciatis similique. Laborum officia neque rerum odio totam consequuntur minima? Inventore in maxime animi nulla quis, corrupti cumque placeat voluptatibus amet, nobis enim dicta repellat. Aperiam modi laborum saepe ex non et iste ab!</p>
         </div>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
